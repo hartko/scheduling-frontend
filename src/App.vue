6f833499e5facf44cf642710e988fcd3c72013f2
Harriette Mae Rodriguez <haryetmey.rodriguez@gmail.com>
2024-01-19 21:32:53 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+</style>
